b420ff86461d69deb227120237e3e2b03b65c011
CCHIABAI <cchiabai@alumno.unlam.edu.ar>
2021-05-30 21:03:33 -0300

    Resolucion basica. (Falta areglar un metodo)

---
+++ src/test/java/Parcial/NoEstaPreparado.java
@@ -0,0 +1,15 @@
+/** 
+ *
+ * Programación Basica 2 - Comision 2900
+ * @author Claudio J. CHIABAI <cchiabai@alumno.unlam.edu.ar>
+ *
+ **/
+
+package Parcial;
+
+/**
+ * 
+ */
+public class NoEstaPreparado extends Throwable {
+
+}
